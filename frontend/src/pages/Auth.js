07c5e63b4e53210fb5f17d600286288e9c047d1d
Mark <learncode23@gmail.com>
2020-01-19 12:04:51 +0000

    Added more to the login process

--- frontend/src/pages/Auth.js
+++ frontend/src/pages/Auth.js
@@ -3,11 +3,21 @@ import React, { Component } from "react";
 import "./Auth.css";
 
 class AuthPage extends Component {
+  state = {
+    isLogin: true
+  };
+
   constructor(props) {
     super(props);
     this.emailEL = React.createRef();
     this.passwordEL = React.createRef();
   }
+
+  switchModeHandler = () => {
+    this.setState(prevState => {
+      return { isLogin: !prevState.isLogin };
+    });
+  };
 
   submitHandler = event => {
     event.preventDefault();
@@ -18,15 +28,50 @@ class AuthPage extends Component {
       return;
     }
 
-    const requestBody = {};
+    let requestBody = {
+      query: `
+        query {
+          login(email: "${email}", password: "${password}") {
+            userId
+            token
+            tokenExpiration
+          }
+        }
+      `
+    };
+
+    if (!this.state.isLogin) {
+      requestBody = {
+        query: `
+          mutation {
+            createUser(userInput: {email: "${email}", password: "${password}"}) {
+              _id
+              email
+            }
+          }
+        `
+      };
+    }
 
     fetch("http://localhost:8000/graphql", {
       method: "POST",
-      body: null,
+      body: JSON.stringify(requestBody),
       headers: {
         "Content-Type": "application/json"
       }
-    });
+    })
+      .then(res => {
+        if (res.status !== 200 && res.status !== 201) {
+          throw new Error("Failed!");
+        }
+        return res.json();
+      })
+      .then(resData => {
+        console.log(resData);
+      })
+      .catch(err => {
+        console.log(err);
+      });
   };
 
   render() {
@@ -42,7 +87,9 @@ class AuthPage extends Component {
         </div>
         <div className="form-actions">
           <button type="submit">Submit</button>
-          <button type="button">Switch to Signup</button>
+          <button type="button" onClick={this.switchModeHandler}>
+            Switch to {this.state.isLogin ? "Signup" : "Login"}}
+          </button>
         </div>
       </form>
     );
